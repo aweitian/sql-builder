1d0dea1b854f8ebc1d070d415a0122cb83d44f64
tiananwei <awei.tian@qq.com>
2017-06-03 15:42:28 +0800

    bind key with value

--- src/MysqlSelectBuild.php
+++ src/MysqlSelectBuild.php
@@ -7,9 +7,9 @@
  */
 namespace Tian\SqlBuild;
 
-class MysqlSelectBuild extends SqlBuild{
+class MysqlSelectBuild extends SqlBuild {
 	public function sql() {
-		//一部分加上空格是为了更严格的SQL
+		// 一部分加上空格是为了更严格的SQL
 		return strtr ( 'SELECT field FROM table join where groupBy having orderBy limit lock', [ 
 				'field' => $this->parseField (),
 				'table' => $this->parseTable (),
@@ -22,28 +22,94 @@ class MysqlSelectBuild extends SqlBuild{
 				' lock' => $this->parseLock () 
 		] );
 	}
-	public function bindFieldExpr($expr) {
-		return $this->bindExpr('field', $expr);
+	/**
+	 *
+	 * @param string $field        	
+	 * @param string $value        	
+	 * @return \Tian\SqlBuild\MysqlSelectBuild
+	 */
+	public function bindField($field, $value = false) {
+		$expr = ':' . $field;
+		$this->bindExpr ( 'values', $expr );
+		if ($value !== false) {
+			$this->bindValue ( $field, $value );
+		}
+		return $this->bindExpr ( 'field', $field );
 	}
-	public function bindJoinExpr($expr) {
-		return $this->bindExpr('join', $expr);
+	/**
+	 *
+	 * @param string $field        	
+	 * @param string $expr        	
+	 * @param string $value        	
+	 * @return \Tian\SqlBuild\MysqlSelectBuild
+	 */
+	public function bindFieldExpr($field, $expr, $value = false) {
+		if ($value !== false) {
+			$this->bindValue ( $field, $value );
+		}
+		return $this->bindExpr ( 'field', $expr);
 	}
-	public function bindWhereExpr($expr) {
-		return $this->bindExpr('where', $expr);
+	/**
+	 *
+	 * @param string $expr
+	 * @param array $bind
+	 * @return \Tian\SqlBuild\MysqlSelectBuild
+	 */
+	public function bindJoinExpr($expr, $bind = []) {
+		return $this->bindExpr ( 'join', $expr, $bind );
 	}
-	public function bindGroupByExpr($expr) {
-		return $this->bindExpr('groupBy', $expr);
+	/**
+	 *
+	 * @param string $expr
+	 * @param array $bind
+	 * @return \Tian\SqlBuild\MysqlSelectBuild
+	 */
+	public function bindWhereExpr($expr, $bind = []) {
+		return $this->bindExpr ( 'where', $expr, $bind );
 	}
-	public function bindHavingExpr($expr) {
-		return $this->bindExpr('having', $expr);
+	/**
+	 *
+	 * @param string $expr
+	 * @param array $bind
+	 * @return \Tian\SqlBuild\MysqlSelectBuild
+	 */
+	public function bindGroupByExpr($expr, $bind = []) {
+		return $this->bindExpr ( 'groupBy', $expr, $bind );
 	}
-	public function bindOrderByExpr($expr) {
-		return $this->bindExpr('orderBy', $expr);
+	/**
+	 *
+	 * @param string $expr
+	 * @param array $bind
+	 * @return \Tian\SqlBuild\MysqlSelectBuild
+	 */
+	public function bindHavingExpr($expr, $bind = []) {
+		return $this->bindExpr ( 'having', $expr, $bind );
 	}
-	public function bindLimitExpr($expr) {
-		return $this->bindExpr('limit', $expr);
+	/**
+	 *
+	 * @param string $expr
+	 * @param array $bind
+	 * @return \Tian\SqlBuild\MysqlSelectBuild
+	 */
+	public function bindOrderByExpr($expr, $bind = []) {
+		return $this->bindExpr ( 'orderBy', $expr, $bind );
 	}
-	public function bindLockExpr($expr) {
-		return $this->bindExpr('lock', $expr);
+	/**
+	 *
+	 * @param string $expr
+	 * @param array $bind
+	 * @return \Tian\SqlBuild\MysqlSelectBuild
+	 */
+	public function bindLimitExpr($expr, $bind = []) {
+		return $this->bindExpr ( 'limit', $expr, $bind );
+	}
+	/**
+	 *
+	 * @param string $expr
+	 * @param array $bind
+	 * @return \Tian\SqlBuild\MysqlSelectBuild
+	 */
+	public function bindLockExpr($expr, $bind = []) {
+		return $this->bindExpr ( 'lock', $expr, $bind );
 	}
 }
--- src/MysqlUpdateBuild.php
+++ src/MysqlUpdateBuild.php
@@ -19,24 +19,57 @@ class MysqlUpdateBuild extends SqlBuild {
 	}
 	/**
 	 *
-	 * @param string $field
-	 * @param string $expr 默认值为 :$field,如果要传递，FIELD前面需要带:
-	 * @return \Tian\SqlBuild\SqlBuild
+	 * @param string $field        	
+	 * @param string $value        	
+	 * @return \Tian\SqlBuild\MysqlUpdateBuild
 	 */
-	public function bindFieldExpr($field, $expr = '') {
-		if (! $expr) {
-			$expr = ':' . $field;
+	public function bindField($field, $value = false) {
+		$expr = ':' . $field;
+		$this->bindExpr ( 'values', $expr );
+		if ($value !== false) {
+			$this->bindValue ( $field, $value );
+		}
+		return $this->bindExpr ( 'field', $field );
+	}
+	/**
+	 *
+	 * @param string $field        	
+	 * @param string $expr        	
+	 * @param string $value        	
+	 * @return \Tian\SqlBuild\MysqlUpdateBuild
+	 */
+	public function bindFieldExpr($field, $expr, $value = false) {
+		if ($value !== false) {
+			$this->bindValue ( $field, $value );
 		}
 		$this->bindExpr ( 'values', $expr );
 		return $this->bindExpr ( 'field', $field );
 	}
-	public function bindWhereExpr($expr) {
-		return $this->bindExpr ( 'where', $expr );
+	/**
+	 * 
+	 * @param string $expr
+	 * @param array $bind
+	 * @return \Tian\SqlBuild\MysqlUpdateBuild
+	 */
+	public function bindWhereExpr($expr, $bind = []) {
+		return $this->bindExpr ( 'where', $expr, $bind );
 	}
-	public function bindOrderByExpr($expr) {
-		return $this->bindExpr('orderBy', $expr);
+	/**
+	 *
+	 * @param string $expr
+	 * @param array $bind
+	 * @return \Tian\SqlBuild\MysqlUpdateBuild
+	 */
+	public function bindOrderByExpr($expr, $bind = []) {
+		return $this->bindExpr ( 'orderBy', $expr, $bind );
 	}
-	public function bindLimitExpr($expr) {
-		return $this->bindExpr ( 'limit', $expr );
+	/**
+	 *
+	 * @param string $expr
+	 * @param array $bind
+	 * @return \Tian\SqlBuild\MysqlUpdateBuild
+	 */
+	public function bindLimitExpr($expr, $bind = []) {
+		return $this->bindExpr ( 'limit', $expr, $bind );
 	}
 }
